95cb891b3202c4cd1f46c4c7e27150f6fcbe16a8
namejone <devnamejone@gmail.com>
2025-02-22 10:01:45 +0600

    master area styled

---
+++ dist/js/scripts.js
@@ -0,0 +1,12 @@
+var heroSLider = new Swiper('.hero-slider', {
+  // Common options
+  loop: true,
+  spaceBetween: 24,
+  slidesPerView: 'auto',
+  speed: 700,
+  centeredSlides: true,
+  autoplay: {
+    delay: 113000,
+    disableOnInteraction: false,
+  },
+})
