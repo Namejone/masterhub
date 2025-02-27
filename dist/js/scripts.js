f0abd8903964271e1cd37a6ed77ac35246ee5dc9
namejone <devnamejone@gmail.com>
2025-02-27 20:55:40 +0600

    another sections

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -10,3 +10,35 @@ var heroSLider = new Swiper('.hero-slider', {
     disableOnInteraction: false,
   },
 })
+var showcaseSlider = new Swiper('.showcase__slider', {
+  // Common options
+  loop: true,
+  spaceBetween: 24,
+  slidesPerView: 2.8,
+  speed: 700,
+  centeredSlides: false,
+  autoplay: {
+    delay: 113000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.s-next',
+    prevEl: '.s-prev',
+  },
+})
+var showcaseSlider = new Swiper('.testimonial__slider', {
+  // Common options
+  loop: true,
+  spaceBetween: 24,
+  slidesPerView: 1,
+  speed: 700,
+  centeredSlides: false,
+  autoplay: {
+    delay: 113000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.t-next',
+    prevEl: '.t-prev',
+  },
+})
